23d5bcf695f520ea528f2db72dafcdfe8e2616d4
Jesús Cervantes <jesus.cervantes0801@gmail.com>
2021-01-02 00:24:07 -0600

    Se termina el Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
 import React, {useEffect, useRef} from 'react';
+import { Link } from 'react-router-dom';
 // @ts-ignore
 import Parallax from 'parallax-js';
-import BackgroundIMG from '../assets/img/background.jpg';
-import Guitar from '../assets/img/guitar.png';
-import Layer1 from '../assets/img/layer1.png';
-import Layer2 from '../assets/img/layer2.png';
-import Layer3 from '../assets/img/layer3.png';
-import Layer4 from '../assets/img/layer4.png';
-import Layer5 from '../assets/img/layer5.png';
-import Layer6 from '../assets/img/layer6.png';
+import Stars from '../assets/img/star.svg';
+import Moonlight from '../assets/img/moonlight.png';
+import Clairo from '../assets/img/clairo.png';
+import Slash from '../assets/img/slash.png';
+import StarBackground from '../assets/img/stars-back.png';
+
 import './styles/Home.css';
 
 const Home = () => {
@@ -25,17 +24,60 @@ const Home = () => {
 
   }, [])
 
+  const onMouseEnter = (e) =>{
+    e.preventDefault();
+    const elements = document.getElementsByName('element');
+
+    elements.forEach((el) =>{
+      el.style.opacity = '1';
+    })
+
+    const stars = document.getElementById('stars');
+    console.log(stars);
+    stars.classList.add('star-shadow');
+    stars.style.opacity = '1';
+  }
+
+  const onMouseLeave = (e) =>{
+    e.preventDefault();
+    const elements = document.getElementsByName('element');
+
+    elements.forEach((el) =>{
+      el.style.opacity = '0.7';
+    })
+
+    const stars = document.getElementById('stars');
+    console.log(stars);
+    stars.classList.remove('star-shadow');
+    stars.style.opacity = '0.2';
+  }
+
   return (
-    <div id="container">
-      <div id="scene" ref={sceneEl}>
-        <div data-depth="1.00"><img src={Layer1} /></div>
-        <div data-depth="0.80"><img src={Layer2} /></div>
-        <div data-depth="0.60"><img src={Layer3} /></div>
-        <div data-depth="0.40"><img src={Layer4} /></div>
-        <div data-depth="0.20"><img src={Layer5} /></div>
-        <div data-depth="0.00"><img src={Layer6} /></div>
+    <React.Fragment>
+      <div className="header"> 
+        <button className="try-button" id="try-button" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+          Try it now!  
+        </button>         
+        <h1 className="title">Reviewsic</h1>           
       </div>
-    </div>
+      <div id="container">
+        <div id="scene" ref={sceneEl} data-scalar-x="10" data-scalar-y="10">
+
+          <div data-depth="0.2"><img src={Stars} className="stars" /></div>
+
+          <div data-depth="0.1"><img src={StarBackground} id="stars"  className="star"/></div>
+
+          <div data-depth="0.3"><img className="moonlight" src={Moonlight} /></div>
+
+          <div data-depth="1" class="clairo-container"><img className="clairo" name="element" src={Clairo} /></div>
+
+          <div data-depth="1" class="slash-container"><img name="element" className="slash" src={Slash} /></div>
+
+                    
+
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
