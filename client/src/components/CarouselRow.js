cebdbb92182d2f59bba8839e3861010eeaf55086
Jesús Cervantes <jesusper9@gmail.com>
2021-04-13 23:34:09 -0500

    El diseno de las filas de carrusel queda arreglado para cuando solo hay un slot disponible, faltaria hacerlo responsive xd

--- client/src/components/CarouselRow.js
+++ client/src/components/CarouselRow.js
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "react-elastic-carousel";
-import Card from './Card'
+import Card from "./Card";
+import './styles/CarouselRow.css'
+
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CarouselRow = (props) => {
 
-  console.log(props)
+  const carousel = React.useRef(null);
+
+  const [index, setIndex] = useState(0)
+
+  const handleClick = (value) => {
+    if(index  + value == -1 || index + value == props.data.songList.length) return
+    setIndex(index + value)
+  }
+
+  useEffect(() => {
+    if(carousel.current)carousel.current.goTo(index)
+  }, [index])
+
+  console.log(props);
   return (
-    <Carousel itemsToShow={2} focusOnSelect={true} renderPagination={({ pages, activePage, onClick }) => {
-      return (<div/>)
-    }} renderArrow={() => {
-      return <div/>
-    }}
-      outerSpacing={0}
-    >
-      {(props.data.songList).map(song => {
-        return (
-          <Card data={song} likedSongs={props.likedSongs}></Card>
-        );
-      })}
-    </Carousel>
+    <div>
+      <div className="carousel-buttons">
+        <div className="buttons-container">
+          <button className="backward-button carousel-button" onClick={() => {
+              handleClick(-1)
+            }}>
+            <FontAwesomeIcon icon={faChevronLeft} className={`carousel-arrows-icon ${index > 0 ? '' : 'disabled-carousel-arrows-icon' }`}/>
+          </button>
+          <button className="forward-button carousel-button" onClick={() => {
+              handleClick(1)
+            }}>
+            <FontAwesomeIcon icon={faChevronRight} className={`carousel-arrows-icon ${index + 1 < props.data.songList.length ? '' : 'disabled-carousel-arrows-icon' }`}/>
+          </button>
+        </div>
+      </div>
+      <Carousel
+        focusOnSelect={true}
+        ref={carousel}
+        // renderPagination={({ pages, activePage, onClick }) => {
+        //   return (<div/>)
+        // }} 
+        renderArrow={() => {
+          return <div/>
+        }}
+      >
+        {props.data.songList.map((song) => {
+          return (
+            <Card data={song} likedSongs={props.likedSongs}></Card>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
 
-export default CarouselRow
+export default CarouselRow;
